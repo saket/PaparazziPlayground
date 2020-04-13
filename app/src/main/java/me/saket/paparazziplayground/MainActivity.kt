022b97d58561d593e041ac9783164168890cef6d
Saket Narayan <saket@squareup.com>
2020-04-12 20:04:52 -0400

    First commit

---
+++ app/src/main/java/me/saket/paparazziplayground/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.saket.paparazziplayground
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    // TODO: Dagger?
+  }
+}
